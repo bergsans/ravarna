5d94b28aac98eeef38509ea163b370cb054193ad
Claes-Magnus Berg <bergclaesmagnus@gmail.com>
2024-12-21 19:07:00 +0100

    add old project

---
+++ precache-manifest.6662de8d0b990884da63f82b399a540b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d781a868176ad8f0a16be6f4ce53c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04b219fc630d9eea36ec",
+    "url": "/static/css/main.23206d89.chunk.css"
+  },
+  {
+    "revision": "1b3985959d78881aac9b",
+    "url": "/static/js/2.890be2da.chunk.js"
+  },
+  {
+    "revision": "04b219fc630d9eea36ec",
+    "url": "/static/js/main.e471a4f7.chunk.js"
+  },
+  {
+    "revision": "cbee8d858c084296ccff",
+    "url": "/static/js/runtime-main.985e6003.js"
+  },
+  {
+    "revision": "62f10d46561ed0b911d42b7510d62d74",
+    "url": "/static/media/CrimsonText-Regular.62f10d46.ttf"
+  },
+  {
+    "revision": "212707c7122923a41b2c05e4e7cf8ddb",
+    "url": "/static/media/HelveticaNeueLight.212707c7.woff"
+  },
+  {
+    "revision": "9d52a8c4fafc763d9a356d8b67d4a2e6",
+    "url": "/static/media/Inconsolata-Regular.9d52a8c4.ttf"
+  },
+  {
+    "revision": "fb8953e4314f6a5ef9731778f42ced85",
+    "url": "/static/media/manaspc.fb8953e4.ttf"
+  }
+]);
